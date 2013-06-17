53fecd054bd68947810fd6e70f53d00cd3cc523c
Morrison Laju <morrelinko@gmail.com>
2013-06-17 19:02:36 +0100

    Started LinkRife Detector Feature

---
+++ src/Linko/Spam/Detector/LinkRife.php
@@ -0,0 +1,14 @@
+<?php
+
+use Linko\Spam\SpamDetectorInterface;
+
+class LinkRife implements SpamDetectorInterface
+{
+    /**
+     * {@inheritDocs}
+     */
+    public function detect($string)
+    {
+
+    }
+}
