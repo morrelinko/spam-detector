bafe4287eccb878b7be9fb23697281ecbe7e6325
Morrison Laju <morrelinko@gmail.com>
2013-06-20 19:00:15 +0100

    Updated example to include LinkRife Detector and fixed some regex definition issues

--- example/example.php
+++ example/example.php
@@ -4,16 +4,25 @@ require_once __DIR__.'/../autoload.php';
 
 use Linko\Spam\SpamFilter;
 use Linko\Spam\Detector\BlackList;
+use Linko\Spam\Detector\LinkRife;
 
+// setup black list detector
 $blackListDetector = new BlackList();
 $blackListDetector->add('superSpammingWebsite.com');
-$blackListDetector->add('[kill|suck]', true);
-$blackListDetector->add('\d{3}\.\d{3}\.\d{3}\.\d{3}', true);
+$blackListDetector->add('kill|suck', true); // regex
+$blackListDetector->add('\d{3}\.\d{3}\.\d{3}\.\d{3}', true); // regex
 
+// setup link rife detector
+$linkRife = new LinkRife();
+
+// setup spam filter father
 $spamFilter = new Linko\Spam\SpamFilter();
-$spamFilter->registerDetector($blackListDetector);
 
-$comment = "Hey dude, your face is suckulent.";
+// register children (o_o )
+$spamFilter->registerDetector($blackListDetector);
+$spamFilter->registerDetector($linkRife);
+
+$comment = "Hey dude, your face is. ";
 
 if($spamFilter->check($comment)->passed()) {
     echo '<h4>Passed</h4>';
